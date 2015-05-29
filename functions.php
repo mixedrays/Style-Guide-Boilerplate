c433dc99d14c625692e5fb2d56b8c8dafada4749
alex.meleshko <alex.meleshko@madebyewave.com>
2015-05-29 17:15:56 +0300

    SG markup rendering refactored, structure refactored

--- functions.php
+++ functions.php
@@ -1,104 +1,121 @@
-  <?php
+<?php
 
-  // Build out URI to reload from form dropdown
-  // Need full url for this to work in Opera Mini
-  $pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+// Build out URI to reload from form dropdown
+// Need full url for this to work in Opera Mini
+$pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
 
-  if (isset($_POST['sg_uri']) && isset($_POST['sg_section_switcher'])) {
-     $pageURL .= $_POST[sg_uri].$_POST[sg_section_switcher];
-     $pageURL = htmlspecialchars( filter_var( $pageURL, FILTER_SANITIZE_URL ) );
-     header("Location: $pageURL");
-  }
+if (isset($_POST['sg_uri']) && isset($_POST['sg_section_switcher'])) {
+    $pageURL .= $_POST[sg_uri] . $_POST[sg_section_switcher];
+    $pageURL = htmlspecialchars(filter_var($pageURL, FILTER_SANITIZE_URL));
+    header("Location: $pageURL");
+}
 
-  // Display title of each markup samples as a select option
-  function listMarkupAsOptions ($type) {
-    $files = array();
-    $handle=opendir('markup/'.$type);
-    while (false !== ($file = readdir($handle))):
-        if(stristr($file,'.html')):
-            $files[] = $file;
-        endif;
-    endwhile;
-
+function listFolderFiles($dir)
+{
+    $files = scandir($dir);
     sort($files);
-    foreach ($files as $file):
-        $filename = preg_replace("/\.html$/i", "", $file);
-        $title = preg_replace("/\-/i", " ", $filename);
-        $title = ucwords($title);
-        echo '<option value="#sg-'.$filename.'">'.$title.'</option>';
-    endforeach;
-  }
 
-  // Display markup view & source
-  function showMarkup($type) {
-    $files = array();
-    $handle=opendir('markup/'.$type);
-    while (false !== ($file = readdir($handle))):
-        if(stristr($file,'.html')):
-            $files[] = $file;
-        endif;
-    endwhile;
+    echo '<ul>';
+    foreach ($files as $file) {
+        if ($file != '.' && $file != '..') {
+            $path = $dir . '/' . $file;
 
-    sort($files);
-    foreach ($files as $file):
-        $filename = preg_replace("/\.html$/i", "", $file);
-        $title = preg_replace("/\-/i", " ", $filename);
-        $documentation = 'doc/'.$type.'/'.$file;
-        echo '<div class="sg-markup sg-section">';
-        echo '<div class="sg-display">';
-        echo '<h2 class="sg-h2"><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h2>';
-        if (file_exists($documentation)) {
-          echo '<div class="sg-doc">';
-          echo '<h3 class="sg-h3">Usage</h3>';
-          include($documentation);
-          echo '</div>';
+            echo '<li>';
+
+            $filename = preg_replace("/\.html$/i", "", $file);
+            $title = preg_replace("/\-/i", " ", $filename);
+            echo '<a href="#sg-' . $filename . '">' . $title . '</a>';
+
+            if (is_dir($path)) {
+                listFolderFiles($path);
+            }
+            echo '</li>';
         }
-        echo '<h3 class="sg-h3">Example</h3>';
-        include('markup/'.$type.'/'.$file);
+    }
+    echo '</ul>';
+}
+
+// Recursively render files in specified directory
+function renderFolderFiles($dir)
+{
+    $files = scandir($dir);
+    sort($files);
+
+    foreach ($files as $file) {
+        if ($file != '.' && $file != '..') {
+            $path = $dir . '/' . $file;
+
+            if (is_dir($path)) {
+                renterTitleFromPath($path);
+                renderFolderFiles($path);
+            } else {
+                renderFile($path);
+            }
+        }
+    }
+}
+
+function renderFile($path)
+{
+    $content = file_get_contents($path);
+    echo '<section class="sg-section">';
+    renterTitleFromPath($path);
+    renderFileDescription($content);
+
+    $content = removeHtmlComments($content);
+    renderFileExample($content);
+    renderFileSource($content);
+    echo '</section>';
+}
+
+function renterTitleFromPath($path)
+{
+    $nestingDepth = (substr_count($path, '/') > 6) ? 6 : substr_count($path, '/');
+    $file = basename($path);
+    $filename = preg_replace("/\.html$/i", "", $file);
+    $title = preg_replace("/\-/i", " ", $filename);
+
+    echo '<h' . $nestingDepth . ' id="sg-' . $filename . '" class="sg-title sg-anchor">' . $title . '</h' . $nestingDepth . '>';
+}
+
+function renderFileDescription($content)
+{
+    $pattern = '/<!--(.*)-->/Uis';
+    preg_match_all($pattern, $content, $matches);
+
+    if (!empty($matches[1])) {
+        echo '<div class="sg-section-description">';
+        foreach ($matches[1] as $match) {
+            echo($match);
+            echo '<br />';
+        }
         echo '</div>';
-        echo '<div class="sg-markup-controls"><a class="sg-btn sg-btn--source" href="#">View Source</a> <a class="sg-btn--top" href="#top">Back to Top</a> </div>';
-        echo '<div class="sg-source sg-animated">';
-        echo '<a class="sg-btn sg-btn--select" href="#">Copy Source</a>';
-        echo '<pre class="prettyprint linenums"><code>';
-        echo htmlspecialchars(file_get_contents('markup/'.$type.'/'.$file));
-        echo '</code></pre>';
-        echo '</div>';
-        echo '</div>';
-    endforeach;
-  }
+    }
+}
 
-  // Recursively render files in specified directory
-  function renderFolderFiles($dir){
-      $files = scandir($dir);
-      sort($files);
+function renderFileExample($content)
+{
+    echo '<div class="sg-section-example">';
+    echo $content;
+    echo '</div>';
+}
 
-      echo '<ul>';
+function renderFileSource($content)
+{
+    echo '<div class="sg-section-source">';
+    echo '<pre>';
+    echo '<code>';
+    echo htmlspecialchars($content);
+    echo '</code>';
+    echo '</pre>';
+    echo '</div>';
+}
 
-      foreach($files as $file){
-          if($file != '.' && $file != '..'){
-              $currentPath = $dir.'/'.$file;
-              $nestingDepth = substr_count($currentPath, '/');
-              $filename = preg_replace("/\.html$/i", "", $file);
-              $title = preg_replace("/\-/i", " ", $filename);
+function removeHtmlComments($content = '')
+{
+    $content = preg_replace('/<!--(.*)-->/Uis', '', $content);
+    $content = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $content);
+    return $content;
+}
 
-              echo '<h'. $nestingDepth .'><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h'. $nestingDepth .'>';
-              echo $currentPath;
-
-              echo '<li style="border: 5px solid red;">';
-
-              if(is_dir($currentPath)) {
-                  renderFolderFiles($currentPath);
-              } else {
-                  include($currentPath);
-              }
-
-              echo '</li>';
-          }
-      }
-
-      echo '</ul>';
-  }
-
-  renderFolderFiles('markup');
-
-  ?>
+?>
